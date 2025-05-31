753dc8f2b1a0e381aebd08bf9f6422f2214696a8
Anton Todorov <tonkisabe@gmail.com>
2025-05-31 16:35:47 +0300

    Delete user_multiply_option().py

--- PySpark/user_multiply_option().py
+++
@@ -1,9 +0,0 @@
-path = "s3://de-40-training-raw/input_training_data/amazon_best_sellers/csv_with_header_pipe/"
-
-df4 = (
-    spark.read
-    .options(delimiter=";", header=True)
-    .csv(path)
-)
-
-df4.show()
