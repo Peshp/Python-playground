3a1b31cb0fc190f4425e300d79f6a7c0be65042f
Anton Todorov <tonkisabe@gmail.com>
2025-06-11 10:09:46 +0300

    Create no_existing_header_pipe.py

---
+++ PySpark/no_existing_header_pipe.py
@@ -0,0 +1,9 @@
+path = "s3://de-40-training-raw/input_training_data/amazon_best_sellers/csv_with_header_pipe/"
+
+df4 = (
+    spark.read
+    .options(delimiter="|", header=False)
+    .csv(path)
+)
+
+df4.show()
