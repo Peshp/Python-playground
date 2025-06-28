3f80c8a8fc5573d921dce6c58919bfd25fa6d2c8
Anton Todorov <tonkisabe@abv.bg>
2025-06-28 20:26:15 +0300

    Update extract_from_multiple_sc.py

--- Pandas/Data Extraction/extract_from_multiple_sc.py
+++ Pandas/Data Extraction/extract_from_multiple_sc.py
@@ -1,13 +1,20 @@
 import os
 import pandas as pd
 
-path = 'data'
+path = './data'
 extension = '.csv'
-files = [file for file in os.listdir(path) if file.endswith(extension)]  
+
+files = [file for file in os.listdir("data") if file.endswith(extension)]
 
 dfs = []
 for file in files:
-    df = pd.read_csv(os.path.join(path, file))  
+    df = pd.read_csv(os.path.join(path, file))
+    df['source_file'] = file
+    df = df.drop_duplicates()
     dfs.append(df)
 
-print(df)
+combined_df = pd.concat(dfs, ignore_index=True)
+output_path = os.path.join(path, 'combined_transformed.csv')
+combined_df.to_csv(output_path, index=False)
+
+print(f"Transformed data loaded into {output_path}")
