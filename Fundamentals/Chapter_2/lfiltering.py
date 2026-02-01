8962a4439d7bdf6112bee6ba0bc1d226f08d92ee
Peshp <todorovanton88@gmail.com>
2026-02-01 16:41:50 +0200

    Update lfiltering.py

--- Fundamentals/Chapter_2/lfiltering.py
+++ Fundamentals/Chapter_2/lfiltering.py
@@ -1,12 +1,17 @@
 import re
 
 name = ["apple", "education", "ice", "ocean", "python", "umbrella"]
+matched = []
 
 pattern = r"^[aeiouAEIOU][a-zA-Z]{4}$"
 
-# Iterate through the names to check the pattern
 for element in name:
     m = re.match(pattern, element)
     if m:
-        print(f"Matched word: {m.group()}")
+        matched.append(element)
+
+print(matched)
+
+# Given a list of strings, use a single list comprehension to extract strings 
+# that meet two criteria: they must be 5 characters AND they must start with a vowel (a, e, i, o, u).
 
