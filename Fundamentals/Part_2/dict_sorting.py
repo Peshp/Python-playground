32b4257dae6f0e7373a09f04f3ff80656c902a35
Peshp <todorovanton88@gmail.com>
2026-02-24 19:14:42 +0200

    Create dict_sorting.py

---
+++ Fundamentals/Part_2/dict_sorting.py
@@ -0,0 +1,8 @@
+employees = [{"name": "A", "salary": 50}, {"name": "B", "salary": 70}, {"name": "C", "salary": 60}]
+
+sorted_list = sorted(employees, key = lambda x: x['salary'], reverse=True)
+
+print(sorted_list)
+
+# Given a list of dictionaries (representing employees), 
+# sort them based on their “salary” in descending order using a lambda function.
